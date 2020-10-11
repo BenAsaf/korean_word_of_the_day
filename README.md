0236848551385364bf0c227d5542a152741842be
Ben Asaf <ToBenAsaf@Gmail.com>
2020-10-11 10:15:20 +0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# korean_word_of_the_day
-Korean Word of the Day utility for Linux terminal
+# Korean Word of the Day
+Korean Word of the Day utility for Linux terminal.<br>
+
+## Example alias for your 'zshrc'/'bashrc'
+>     function kwotd() { path/to/script/venv/bin/python path/to/script/kwotd.py; } 
+>     kwotd  # run it once.
+
+## Requirements
+Virtual Environment with: Python3, BeautifulSoup4, lxml, termcolor
+
+## How it works
+Parses the website https://www.transparent.com/word-of-the-day/ using the RSS feed for the desired language.
+The results of the parsing are saved in 'tmp' folder.
+It checks whether the data stored is from yesterday, if it is, it parses the website again and stores the new data.
+We're doing this in-order not to spam the website everytime we open a terminal.
+
+
+## Notes 
+
+This script can be easily adapted to other languages from the website https://www.transparent.com/word-of-the-day/ using the RSS feed, just change the URL inside the script.<br>
+Note, some other languages will require the parsing to be different since they add more information, for instance for Japanese where they add "Romaji example"
+As long as the structure is the same: "Word", "Part of Speech", "Example Sentence", "Sentence Meaning" all you have to do is change the URL.<br>
+
+Examples for languages with the same structure for easy URL swap:
+https://www.transparent.com/word-of-the-day/today/german.html
+https://www.transparent.com/word-of-the-day/today/italian.html
+
+Remember that you need the RSS URL, it is obtainable at the top part of the website in the text:
+>>Italian Word of the Day<br>
+Visit this page each day to learn new Italian vocabulary, or get new words delivered to you every day via email or **_RSS_** feed.
+
+## WIP:
+EXtracting the Audio links so it's available.
+Preferably maybe embed a button in the Terminal using plugin.
+
+Extracting the links might require Selenium because there is a delay and an iFrame when loading that part.
+
+![](./screenshot/screenshot.png)
---
+++ kwotd.py
@@ -0,0 +1,111 @@
+import requests
+import shutil
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os
+from tempfile import gettempdir
+import termcolor
+
+URL = "http://feeds.feedblitz.com/korean-word-of-the-day"
+
+# URL_FOR_AUDIO = "https://www.transparent.com/word-of-the-day/today/korean.html"  # TODO WIP
+# URL_FOR_AUDIO = "https://wotd.transparent.com/widget/?lang=korean&_ga=2.191404842.1871955312.1601750216-1232972085.1601537782"  # TODO WIP
+
+TITLE = f"{termcolor.colored('Korean', 'cyan')} " \
+        f"{termcolor.colored('Word', 'yellow')} " \
+        f"{termcolor.colored('of', 'green')} " \
+        f"{termcolor.colored('the', 'magenta')} " \
+        f"{termcolor.colored('Day', 'red')}"
+PART_OF_SPEECH = termcolor.colored("Part of speech:", color=None, attrs=["bold"])
+EXAMPLE_SENTENCE = termcolor.colored("Example sentence:", color=None, attrs=["bold"])
+SENTENCE_MEANING = termcolor.colored("Sentence meaning:", color=None, attrs=["bold"])
+LEFT_BRACKET = termcolor.colored("[", color="red", attrs=["bold"])
+RIGHT_BRACKET = termcolor.colored("]", color="red", attrs=["bold"])
+
+
+TIME_FORMAT_FROM_WEB = "%a, %d %b %Y %H:%M:%S %Z"
+TIME_FORMAT = "%a, %d %b %Y %H:%M:%S"
+
+BASE_SAVE_PATH = os.path.join(gettempdir(), "korean_word_of_the_day")
+PATH_TO_SAVE_TXT = os.path.join(BASE_SAVE_PATH, "korean_word_of_the_day.txt")
+
+
+def extract_word(raw_content):
+    s = raw_content.rindex("<title>")
+    e = raw_content.rindex("</title>")
+    current = raw_content[s:e]
+    return current[current.find("CDATA")+5:-2][1:-1]
+
+
+def is_still_relevant(now, saved):
+    return not(now.day > saved.day or now.month > saved.month or now.year > saved.year)
+
+
+# def get_audio_links():  # TODO WIP
+#     _response = requests.get(URL_FOR_AUDIO)
+#     response_content = _response.content.decode()
+#     soup = BeautifulSoup(response_content, "lxml")
+#     print()
+
+
+def download():
+    global TITLE, URL
+    _response = requests.get(URL)
+    response_content = _response.content.decode()
+
+    soup = BeautifulSoup(response_content, "lxml")
+
+    word = extract_word(response_content)
+    all_tr = soup.find_all("tr")
+    part_of_speech = all_tr[0].text.replace("\n", "").split(":")[1]
+    example_sentence = all_tr[1].text.replace("\n", "").split(":")[1]
+    sentence_meaning = all_tr[2].text.replace("\n", "").split(":")[1]
+    timestamp = datetime.strptime(soup.pubdate.text, "%a, %d %b %Y %H:%M:%S %Z")
+
+    # audio_links = get_audio_links()
+
+    msg = word + "\n" + part_of_speech + "\n" + example_sentence + "\n" + sentence_meaning
+    if os.path.exists(PATH_TO_SAVE_TXT):
+        os.remove(PATH_TO_SAVE_TXT)
+    with open(PATH_TO_SAVE_TXT, 'w') as fp:
+        fp.write(str(timestamp.strftime(TIME_FORMAT).strip()) + "\n")
+        fp.write(msg)
+        fp.write("\n")
+    return word, part_of_speech, example_sentence, sentence_meaning
+
+
+def main():
+
+    try:
+        current_ts = datetime.now()
+        os.makedirs(BASE_SAVE_PATH, exist_ok=True)
+        if os.path.exists(PATH_TO_SAVE_TXT):
+            with open(PATH_TO_SAVE_TXT, 'r') as fp:
+                timestamp = fp.readline().strip()
+                word = fp.readline().strip()
+                part_of_speech = fp.readline().strip()
+                example_sentence = fp.readline().strip()
+                sentence_meaning = fp.readline().strip()
+                # word_audio = fp.readline().strip()
+                # sentence_audio = fp.readline().strip()
+            timestamp = datetime.strptime(timestamp, TIME_FORMAT)
+            if is_still_relevant(current_ts, timestamp):
+                pass
+            else:
+                shutil.rmtree(BASE_SAVE_PATH)
+                os.makedirs(BASE_SAVE_PATH)
+                word, part_of_speech, example_sentence, sentence_meaning = download()
+        else:
+            word, part_of_speech, example_sentence, sentence_meaning = download()
+        print(TITLE)
+        print(f"{LEFT_BRACKET}{word}{RIGHT_BRACKET}")
+        print(PART_OF_SPEECH, part_of_speech)
+        print(EXAMPLE_SENTENCE, example_sentence)
+        print(SENTENCE_MEANING, sentence_meaning)
+
+    except Exception as e:
+        print(TITLE, e)
+
+
+if __name__ == '__main__':
+    main()
